37fbf9a211a9f00a380afeeadaaac43d1a9ee1a9
shijin <shijin-benny>
2022-03-26 12:18:24 +0530

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
     return (
         <>
-            <div className='h-screen m-2 lg:m-24'>
+            <div className='h-screen m-2 lg:ml-8 lg:mt-3'>
                 <div className='max-w-full space-auto lg:space-x-2 text-gray-500 lg:py-5'>
                     <span>Home</span>
                     <span class="material-icons text-sm">
@@ -15,31 +15,68 @@ function Home() {
                     </span>
                     <span>Organic Bread</span>
                 </div>
-                <div className='max-w-full flex flex-col'>
-                    <div className='text-gray-500'>
-                        <h1 className='text-2xl lg:text-5xl'>Dave's Killer  Bread Organic 21 Whole Grain Bread</h1>
+                <div className='grid grid-cols-4 grid-rows-2'>
+                    <div className='col-span-1 row-span-2 px-8'>
+                        <div className='w-80 h-96 border flex justify-center flex-col'>
+                            <div className='flex justify-center'>
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToutmx3CCfOr2HJ_t6MhU8EakxSrezuoW63w&usqp=CAU" alt="" />
+                            </div>
+                            <div className=' w-14 flex justify-between space-x-8 ml-5'>
+                                <img className='border border-blue-600' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToutmx3CCfOr2HJ_t6MhU8EakxSrezuoW63w&usqp=CAU" alt="" />
+                                <img className='border border-blue-600' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToutmx3CCfOr2HJ_t6MhU8EakxSrezuoW63w&usqp=CAU" alt="" />
+                                <img className='border border-blue-600' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToutmx3CCfOr2HJ_t6MhU8EakxSrezuoW63w&usqp=CAU" alt="" />
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex flex-row text-yellow-500 items-center space-x-1'>
-                        <span className="material-icons text-5xl">
-                            star_rate
-                        </span>
-                        <span className="material-icons text-5xl">
-                            star_rate
-                        </span>
-                        <span className="material-icons text-5xl">
-                            star_rate
-                        </span>
-                        <span className="material-icons text-5xl">
-                            star_rate
-                        </span>
-                        <span>5 Reviews</span>
+                    <div className='col-span-2 flex flex-col'>
+                        <h1 className='text-3xl font-semibold'>Brake Pads</h1>
+                        <div className='text-blue-700'>
+                            <span className="material-icons text-4xl text-center">
+                                star_rate
+                            </span>
+                            <span className="material-icons text-4xl">
+                                star_rate
+                            </span>
+                            <span className="material-icons text-4xl">
+                                star_rate
+                            </span>
+                            <span className="material-icons text-4xl">
+                                star_rate
+                            </span>
+                            <span className='ml-2 font-bold'>1088 Reviews</span>
+                            <span className='ml-4 font-bold'>675 Answered Questions</span>
+                        </div>
+                        <div className='m-4'>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tempora labore earum officia sapiente nam ratione, totam doloremque! In provident distinctio assumenda necessitatibus ea ut laborum asperiores iusto dicta libero?</p>
+                            <p className='text-blue-500 m-3'>More Details</p>
+                        </div>
+                        <div className='inline space-x-4'>
+                            <span className='text-4xl'>$87.00</span>
+                            <span>Free Shipping</span>
+                            <span className="material-icons text-blue-600 text-4xl">
+                                import_export
+                            </span>
+                            <span className='text-blue-600'>Add to Compare</span>
+                        </div>
+                        <div className='p-4 flex flex-row space-x-2'>
+                            <div><span className='text-2xl text-gray-500'>Qty</span></div>
+                            <div className='w-32 h-10 border flex justify-between items-center p-3'>
+                            <button className='text-3xl'>-</button>
+                            <span>0</span>
+                            <button className='text-3xl'>+</button>
+                            </div>
+                           
+                        </div>
+                        <div className='space-x-8'>
+                            <button className='bg-blue-400 px-4 py-2 font-bold text-white rounded-lg'>Add to cart</button>
+                            <button className='bg-blue-800 px-4 py-2 font-bold text-white rounded-lg'>Add to cart</button>
+                        </div>
+                        <div className='m-3'>
+                       <span>Share</span>
+                        </div>
                     </div>
+                    <div>1</div>
 
-                </div>
-                <div className='max-w-full flex flex-row'>
-                    <div className='flex items-center'>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNTvcxEsKg19aTM9NmESGkTVeeuCEZ_gdEzQ&usqp=CAU" alt="product" />
-                    </div>
                 </div>
             </div>
         </>
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,34 +3,41 @@ import React from 'react'
 function Navbar() {
   // const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className='grid lg:grid-cols-4 py-3 h-14'>
-      <div className='col-span-1 flex items-center justify-center'>
-        <img className='w-10' src="https://i.pinimg.com/originals/b6/79/54/b67954a7113e1fb688b43eb51477fc3a.jpg" alt="logo" />
-        <h1 className='text-3xl font-semibold text-green-600'>Company<span className='text-gray-500'>logo</span></h1>
+    <nav className='grid grid-col-2 lg:grid-cols-5 py-1 border-b border-gray-600'>
+      <div className='flex flex-row items-center col-span-1 lg:flex lg:items-center lg:justify-center'>
+        <img className='w-10' src="https://www.seekpng.com/png/detail/267-2675194_00106-3d-company-logos-design-free-logo-online.png" alt="logo" />
+        <h1 className='text-3xl font-semibold text-blue-800'>Company<span className='text-gray-500 font-light'>Logo</span></h1>
       </div>
-      <div className='col-span-2 flex items-center justify-center flex-grow'>
-        <input className='w-full h-10 px-4 border border-black rounded-l-lg' type="text" placeholder='Search' />
-        <button className='bg-gray-100 flex items-center justify-center rounded-r-lg h-10 w-14 border text-white border-gray-400 border-l-0 bg-gray-700'>
-          <i class="material-icons">search</i>
-        </button>
+      <div className='hidden lg:block lg:col-span-2 lg:flex lg:items-center justify-between text-gray-400 font-semibold'>
+        <span>category 1</span>
+        <span>category 2</span>
+        <span>category 3</span>
+        <span>category 4</span>
+        <span>category 5</span>
       </div>
-      <div className='col-span-1 flex justify-end space-x-8 mr-5'>
-      <span class="material-icons text-5xl text-green-600">
-          account_circle
-        </span>
-        <span class="material-icons text-5xl text-green-600">
-          shopping_cart_checkout
-        </span>
-      </div>
-      <div className='hidden col-span-4 lg:block bg-gray-600 w-full  h-12 lg:flex lg:justify-between items-center px-20 text-white mt-5 '>
-        <span>Bakery</span>
-        <span>Nuts & seeds</span>
-        <span>Vegetables</span>
-        <span>cheeses</span>
-        <span>Sea Food</span>
-        <span>Dairy</span>
-        <span>Meat</span>
-        <span>Snaks</span>
+      <div className='col-span-1 lg:col-span-2 p-5 flex flex-col max-w-full items-end'>
+        <div className='flex justify-between space-x-4'>
+          <span>Ship to : <span className='text-blue-500'>798 Davis lane</span></span>
+          <div className='flex flex-grow'>
+            <span className="material-icons text-4xl">
+              account_circle
+            </span>
+            <p className='text-gray-500'>My Account</p>
+          </div>
+          <div className='flex flex-grow'>
+            <span className="material-icons text-4xl">
+              add_shopping_cart
+            </span>
+            <p className='text-gray-500'>Cart</p>
+          </div>
+
+        </div>
+        <div className='w-2/5 lg:min-w-full p-4 lg:flex lg:flex-grow lg:items-center lg:justify-end'>
+          <input className='py-2 px-2 border border-black rounded-l-lg' type="text" name="" id="" placeholder='Search' />
+          <span className="material-icons bg-blue-700 py-2 px-4 text-white text-2xl rounded-r-lg">
+            search
+          </span>
+        </div>
       </div>
     </nav>
   )
